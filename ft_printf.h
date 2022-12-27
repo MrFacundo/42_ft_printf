ad4da284d35ec8ebc7b4e2e485e2be5ec58ee97e
Facundo Troitero <ftroiter@c1r8s1.42lisboa.com>
2022-12-27 15:20:57 +0000

    norm libft.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,18 +6,18 @@
 /*   By: ftroiter <ftroiter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 21:39:39 by ftroiter          #+#    #+#             */
-/*   Updated: 2022/12/22 21:31:15 by ftroiter         ###   ########.fr       */
+/*   Updated: 2022/12/27 15:20:11 by ftroiter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
+# include <stdarg.h>
 
 /* ft_printf.c */
-int	ft_printf(const char *str, ...);
-int parse_specifier(char *str, va_list args);
+int		t_printf(const char *str, ...);
+int		parse_specifier(char *str, va_list args);
 
 /* ft_printf_functions.c */
 int		ft_printf_char(char c);
@@ -31,6 +31,5 @@ int		ft_num_length(int n, int base);
 int		ft_unsigned_num_length(unsigned long long n, int base);
 void	ft_putfnbr_base_fd(long long n, int base, int uppercase, int fd);
 void	ft_putunbr_base_fd(unsigned long long n, int base, int fd);
-
 
 #endif
